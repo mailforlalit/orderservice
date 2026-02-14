ff0501face87431e10be763a0b9966be9274df0b
LALIT GUPTA <39664049+mailforlalit@users.noreply.github.com>
2026-02-14 21:37:41 +0530

    update

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,8 +1,23 @@
 package com.example.orderservice.controller;
 
+import com.example.orderservice.dto.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class OrderController {
+
+    private RestTemplate restTemplate;
+
+    public OrderController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/orders/product/{id}")
+    public Product getOrderById(@PathVariable String id) {
+        return restTemplate.getForObject("http://productservice/products/" + id, Product.class);
+    }
 
 }
