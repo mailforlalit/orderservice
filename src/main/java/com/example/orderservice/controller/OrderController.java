a9c40331c0cc5122568852615d775955b2e255f6
LALIT GUPTA <39664049+mailforlalit@users.noreply.github.com>
2026-02-14 21:57:24 +0530

    Web Client

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -5,19 +5,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class OrderController {
 
     private RestTemplate restTemplate;
 
-    public OrderController(RestTemplate restTemplate) {
+    private WebClient webClient;
+
+    public OrderController(RestTemplate restTemplate, WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
         this.restTemplate = restTemplate;
     }
 
     @GetMapping("/orders/product/{id}")
     public Product getOrderById(@PathVariable String id) {
         return restTemplate.getForObject("http://productservice/products/" + id, Product.class);
+    }
+
+    @GetMapping("/orders/product/web/{id}")
+    public Product getWebOrderById(@PathVariable String id) {
+        return webClient.get().uri("http://productservice/products/" + id)
+                .retrieve().bodyToMono(Product.class).block();
     }
 
 }
