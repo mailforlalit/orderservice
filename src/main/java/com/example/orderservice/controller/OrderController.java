4cd32931a3d549b5cbb0ab0c8b0726e29b621215
LALIT GUPTA <39664049+mailforlalit@users.noreply.github.com>
2026-02-14 22:20:41 +0530

    Feign Client

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,6 +1,9 @@
 package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.Product;
+import com.example.orderservice.feign.OrderFeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClientBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +13,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RestController
 public class OrderController {
 
+    private OrderFeignClient orderFeignClient;
     private RestTemplate restTemplate;
-
     private WebClient webClient;
 
-    public OrderController(RestTemplate restTemplate, WebClient.Builder webClientBuilder) {
+    public OrderController(RestTemplate restTemplate, WebClient.Builder webClientBuilder, OrderFeignClient orderFeignClient) {
         this.webClient = webClientBuilder.build();
+        this.orderFeignClient = orderFeignClient;
         this.restTemplate = restTemplate;
+
     }
 
     @GetMapping("/orders/product/{id}")
@@ -28,6 +33,11 @@ public class OrderController {
     public Product getWebOrderById(@PathVariable String id) {
         return webClient.get().uri("http://productservice/products/" + id)
                 .retrieve().bodyToMono(Product.class).block();
+    }
+
+    @GetMapping("/orders/product/feign/{id}")
+    public Product getFeignOrderById(@PathVariable Long id) {
+        return orderFeignClient.getProductById(id);
     }
 
 }
---
+++ src/main/java/com/example/orderservice/feign/OrderFeignClient.java
@@ -0,0 +1,13 @@
+package com.example.orderservice.feign;
+
+import com.example.orderservice.dto.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "productservice")
+public interface OrderFeignClient {
+
+    @GetMapping("/products/{id}")
+    Product getProductById(@RequestParam("id") long id);
+}
